add6c9606a38c1154d3f54ddbc9eab4eb448de5d
BrunoCesarAngst <bruno.angst@rede.ulbra.br>
2023-04-22 14:22:21 -0300

    Initial Commit

---
+++ mysys/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  mysys
+//
+//  Created by Bruno Angst on 22/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mysys/mysysApp.swift
@@ -0,0 +1,17 @@
+//
+//  mysysApp.swift
+//  mysys
+//
+//  Created by Bruno Angst on 22/04/23.
+//
+
+import SwiftUI
+
+@main
+struct mysysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
